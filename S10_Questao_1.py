b5ab341c8a7a6973638e0ea65d9f22e9da0aed5b
joaorabelo33 <143022690+joaorabelo33@users.noreply.github.com>
2023-10-22 17:45:58 -0300

    Add files via upload

---
+++ S10_Questao_1.py
@@ -0,0 +1,5 @@
+#Escreva um programa que mostre todos os números inteiros de 1 a 50 (um por linha).
+
+for j in range(50):
+    print(j + 1)
+    
---
+++ S10_Questao_4.py
@@ -0,0 +1,10 @@
+#Escreva um programa que gere a seguinte sequência:
+#10, 20, 30, 40, ..., 990, 1000.
+#Considere a separação dos números por vírgula seguido de espaço em branco e o ponto no final da sequência.
+
+sequencia = ''
+for i in range(10 , 1010, 10):
+    sequencia += str(i) + ',' + ' '  
+sequencia = sequencia.strip()
+sequencia = sequencia[:-1]
+print(f'{sequencia}.')
